ade2d76ef8b993e365057b1a0aa16d7e07d2c42a
YEAHx4 <developerstarlight@gmail.com>
2022-07-08 15:12:26 +0900

    fix opencv error

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
-brew install python-tk
-brew install pillow
+if [[ $(uname -p) == 'arm' ]]; then
+  ibrew install python-tk
+  ibrew install pillow
+else
+  brew install python-tk
+  brew install pillow
+fi
+
+pip3 uninstall opencv-python
+pip3 uninstall opencv-contrib-python
+pip3 uninstall opencv-contrib-python-headless
 
 pip3 install -r requirements.txt
